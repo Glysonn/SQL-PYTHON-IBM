6251048691933d08a29e94c9a3f412465d052ab0
Glyson <83619472+Glysonn@users.noreply.github.com>
2022-05-16 09:45:00 -0300

    Update instrutores.sql
    Apenas arrumando uma linha de comentário.

--- SEMANA 1/instrutores.sql
+++ SEMANA 1/instrutores.sql
@@ -1,8 +1,8 @@
 ---Tarefa 0: solte a tabela INSTRUTOR a partir do banco de dados caso ela já exista, para que possamos começar do zero.
 CREATE DATABASE semana1;
 
-/* Tarefa 1: crie a table INSTRUTOR como definido acima. Defina o ins_id como a chave primária
-e certifique-se de que o sobrenome e o nome não sejam nulos.*/
+---Tarefa 1: crie a table INSTRUTOR como definido acima. Defina o ins_id como a chave primária
+---e certifique-se de que o sobrenome e o nome não sejam nulos.
 USE semana1;
 CREATE TABLE INSTRUTOR (
     ins_id int PRIMARY KEY,
