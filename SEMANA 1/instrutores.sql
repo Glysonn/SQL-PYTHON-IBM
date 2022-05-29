f3417a2ab894860eb0c655f02317f1e2d22efb98
Glyson <kauaglyson@gmail.com>
2022-05-29 18:04:17 -0300

    Arquivos CSV e Script Create Table
    Adicionando arquivos necessários para realizar os exercícios.

--- SEMANA 1/instrutores.sql
+++ SEMANA 1/instrutores.sql
@@ -1,7 +1,7 @@
 ---Tarefa 0: solte a tabela INSTRUTOR a partir do banco de dados caso ela já exista, para que possamos começar do zero.
 CREATE DATABASE semana1;
 
-/* Tarefa 1: crie a table INSTRUTOR como definido acima. Defina o ins_id como a chave primária
+/* ---Tarefa 1: crie a table INSTRUTOR como definido acima. Defina o ins_id como a chave primária
 e certifique-se de que o sobrenome e o nome não sejam nulos.*/
 USE semana1;
 CREATE TABLE INSTRUTOR (
