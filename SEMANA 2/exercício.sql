da02508c5a14938b9f56080898d37c95f487563d
Glyson <kauaglyson@gmail.com>
2022-05-29 18:35:11 -0300

    Query 2
    Recupere todos os funcionários que nasceram durante a década de 1970.

--- SEMANA 2/exercício.sql
+++ SEMANA 2/exercício.sql
@@ -3,3 +3,7 @@ SELECT F_NAME, L_NAME
 FROM employees 
 WHERE ADDRESS LIKE "%Elgin,IL%"
 
+---Query 2: Recupere todos os funcionários que nasceram durante a década de 1970.
+SELECT F_NAME, L_NAME
+FROM employees
+WHERE B_DATE LIKE "197%"
